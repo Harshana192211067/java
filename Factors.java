54828f6edf0a1cc5f610b729fc2d9dfb48c97d9a
Harshana192211067 <123436487+Harshana192211067@users.noreply.github.com>
2024-04-20 20:54:38 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factors {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int num = scanner.nextInt();
+
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of factors = " + count);
+    }
+}
