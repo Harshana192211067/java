f338da719f75bccee4090d3fa5e71ea70cd895e8
Harshana192211067 <123436487+Harshana192211067@users.noreply.github.com>
2024-04-20 12:34:00 +0530

    Add files via upload

---
+++ rectangle_pattern.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class rectangle_pattern{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+int n = scanner.nextInt();
+int m=scanner.nextInt();
+int i,j;
+for(i=0;i<n;i++){
+for(j=0;j<m;j++){
+System.out.print(" * ");}
+System.out.println();
+
+}}}
