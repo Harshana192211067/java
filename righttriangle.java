4c760069d6af31161b43bda0cb7b081b5ffb11f2
Harshana192211067 <123436487+Harshana192211067@users.noreply.github.com>
2024-04-20 12:06:48 +0530

    Add files via upload

---
+++ righttriangle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class righttriangle{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+int n = scanner.nextInt();
+int i,j;
+for(i=0;i<n;i++){
+for(j=0;j<i+1;j++){
+System.out.print(" * ");}
+System.out.println();
+
+}}}
